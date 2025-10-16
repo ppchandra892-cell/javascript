22de7c93819a50bfc325687705c87f76d63fa249
ppchandra892-cell <ppchandra892@gmail.com>
2025-10-16 10:24:41 +0530

    arrays

---
+++ arrays/eight.js
@@ -0,0 +1,10 @@
+
+let employees=[
+    {ename:'chandu',eid:20,esal:2454,erole:"hr"},
+    {ename:'prakash',eid:30,esal:24454,erole:"manger"},
+     {ename:'ramu',eid:40,esal:24544,erole:"assositive"}
+    
+]
+console.log(employees[0].ename)
+console.log(employees[1].ename)
+console.log(employees[2].ename)
---
+++ arrays/nine.js
@@ -0,0 +1,11 @@
+
+let eid=[20,30,40]
+let employees=[
+    {ename:'chandu',eid:20,esal:2454,erole:"hr"},
+    {ename:'prakash',eid:30,esal:24454,erole:"manger"},
+     {ename:'ramu',eid:40,esal:24544,erole:"assositive"}
+    
+]
+for(let emp of employees){
+    console.log(emp.ename)
+}
